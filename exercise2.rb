2ed42f1c81000d0f721ade138a536f9d2362c217
Paul Walsh <twentytwentyk@live.com>
2017-01-16 14:27:11 -0500

    add exercise2.rb

---
+++ exercise2.rb
@@ -0,0 +1,11 @@
+#1. I want to leave a decent tip. Multiply ($)55 by 15% or 0.15, the result is your tip amount#
+  puts 55 * 0.15
+
+#2. I want to print an integer to a string #
+puts "5" + 5.to_s
+
+#I want to output the result of 45628 multiplied by 7839 in a sentence#
+puts "Wouldn't it be cool if I added 45628 + 7839 and the answer was #{45628 + 7839} ? I think that would be great personally."
+
+# && = and, so... (if 10 is less than 20 | and | 30 is less than 20[its not]) OR 10 does not equal 11. Side L is false, or side 2 is true. Thus the answer must be True.#
+(10 < 20 && 30 < 20) || !(10 == 11)
